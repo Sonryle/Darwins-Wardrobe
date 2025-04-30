143e3e6c5fa82acc0aba59cc7db7cbbcf16a8429
Joseph <cathcartj08@gmail.com>
2025-04-30 14:54:57 +0100

    Cleaned up files & added custom icon

--- Darwins-Wardrobe/Android/MyNativeCApplication/app/src/main/cpp/main.cpp
+++ Darwins-Wardrobe/Android/MyNativeCApplication/app/src/main/cpp/main.cpp
@@ -8,6 +8,6 @@ extern "C" {
 #include <game-activity/native_app_glue/android_native_app_glue.c>
 
 void android_main(struct android_app *pApp) {
-    __android_log_print(ANDROID_LOG_DEBUG, "AO", "%s", "Heyy");
+    __android_log_print(ANDROID_LOG_DEBUG, "AO", "%s", "Welcome to C++!");
 }
 }
